8b86520ed401d63e1b98fac044e476d0e1fa8a7a
Gusta Mnds <gustavo17mendes21112004@gmail.com>
2024-07-11 16:12:20 -0300

    Update main.js

--- loja/js/main.js
+++ loja/js/main.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function(){
   var header = document.querySelector('#header')
-  header.classList.toggle('rolagem', window.scrollY > 0)
+  header.classList.toggle('rolagem', window.scrollY > 500)
 })
